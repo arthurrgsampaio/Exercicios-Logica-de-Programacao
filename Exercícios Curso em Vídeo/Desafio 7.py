cf1c4d17cbb79cb5591a901c00f054fc6def1892
Arthur Rodrigues <arthur.rgs03@gmail.com>
2023-02-02 23:11:17 -0300

    desafio 7

---
+++ Exercícios Curso em Vídeo/Desafio 7.py
@@ -0,0 +1,5 @@
+#Desafio 7
+n1 = float(input('Digite a sua primeira nota: '))
+n2 = float(input('Digite a sua segunda nota: '))
+nm = (n1+n2)/2
+print('A média da sua nota entre as duas provas foi de: {:.1f}'.format(nm))
