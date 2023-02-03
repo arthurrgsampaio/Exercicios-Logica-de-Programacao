b43fedb06fceba9643d6cef13074a08d72b7edda
Arthur Rodrigues <arthur.rgs03@gmail.com>
2023-02-02 23:26:14 -0300

    desafio 6

---
+++ Exercícios Curso em Vídeo/Desafio 6.py
@@ -0,0 +1,5 @@
+#Desafio 6
+nd = n*2
+nt = n*3
+nrq = n**(1/2)
+print('O dobro do número digitado é: {}; o triplo: {}; e a raiz quadrada com até no máximo três casa após o ponto: {:.3f}'.format(nd, nt, nrq))
